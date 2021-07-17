b4869c458d1146d4e537783c198097bb66267954
huntleyfrancesta <huntleyfrancesta@gmail.com>
2021-07-16 21:15:38 -0400

    done

---
+++ lib/project_backer.rb
@@ -0,0 +1,25 @@
+# require "backer.rb"
+# require "project.rb"
+
+require 'pry'
+
+class ProjectBacker
+
+    attr_reader :project, :backer
+
+    @@all = []
+
+    def initialize(project, backer)
+        @project = project
+        @backer = backer
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+end
+# binding.pry
